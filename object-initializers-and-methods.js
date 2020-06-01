d2cc1ab1bbc277f23a8c031108a2c082f400d8ef
Wesley Rou <wesleyrou@gmail.com>
2020-06-01 14:29:47 -0700

    even more stuff

--- object-initializers-and-methods.js
+++ object-initializers-and-methods.js
@@ -71,7 +71,7 @@ function decodeWords(string){
 }
 console.log(decodeWords('craft block argon meter bells brown croon droop'));
 
-function createCharacter(name,nickname,race,origin,attack,defense){
+function createCharacter(name,nickname,race,origin,attack,defense,weapon){
   return{
     name,
     nickname,
@@ -79,8 +79,9 @@ function createCharacter(name,nickname,race,origin,attack,defense){
     origin,
     attack,
     defense,
+    weapon,
     describe: function(){
-      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);    
+      console.log(`${this.name} is a ${this.race} from ${this.origin} who uses a ${this.weapon}.`);    
     },
     evalueateFight: function(character){
       let y=0;
@@ -101,13 +102,48 @@ function createCharacter(name,nickname,race,origin,attack,defense){
 }
 
 let characters=[
-  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
-  createCharacter('Bilbo Baggins','bilbo', 'Hobbit', 'The Shire', 2, 1),
-  createCharacter('Frodo Baggins','frodo', 'Hobbit', 'The Shire', 3, 2),
-  createCharacter('Aragorn son of Arathorn','aragorn', 'Man', 'Dunnedain', 6, 8),
-  createCharacter('Legolas','legolas', 'Elf', 'Woodland Realm', 8, 5)
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6,'wizard staff'),
+  createCharacter('Bilbo Baggins','bilbo', 'Hobbit', 'The Shire', 2, 1, 'The Ring'),
+  createCharacter('Frodo Baggins','frodo', 'Hobbit', 'The Shire', 3, 2, 'String and Barrow Blade'),
+  createCharacter('Aragorn son of Arathorn','aragorn', 'Man', 'Dunnedain', 6, 8, 'Anduril'),
+  createCharacter('Legolas','legolas', 'Elf', 'Woodland Realm', 8, 5, 'Bow and Arrow')
 ];
 
-characters.push('Arwen Undomiel','arwen', 'Half-Elf', 'Rivendell',5,4);
+characters.push(createCharacter('Arwen Undomiel','arwen', 'Half-Elf', 'Rivendell',5,4, 'Hadhafang'));
 
+characters.find((character)=>character.nickname==='aragorn').describe();
 
+let hobbits=characters.filter((character)=>character.race==='Hobbit');
+console.log(hobbits);
+
+let above5Attack=characters.filter((character)=>character.attack>5);
+console.log(above5Attack);
+
+//8.
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query){
+  const found=arr.find((hero)=function(){
+    let result=true;
+    for(prop in query){
+      if(!(prop in hero)){
+        result=false;
+      }
+    }
+    if(result=true){
+      return hero;
+    }
+  });
+  return found;
+}
+
+findOne(HEROES, { id: 1 });
