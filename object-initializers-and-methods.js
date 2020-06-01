2e4ddcc3491ebb82efa9edd4c3f69a3b1666ed39
Wesley Rou <wesleyrou@gmail.com>
2020-06-01 08:36:39 -0700

    initial commit

---
+++ object-initializers-and-methods.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let loaf = {
+  flour:300,
+  water:210,
+  hydration(){
+    return this.water/this.flour*100;
+  }
+
+};
+
+console.log(loaf.flour, loaf.water);
+console.log(loaf.hydration());
